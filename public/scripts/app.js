cc668f833e4d3fbaed772453e68c03c9e4ca2ba6
Kevin Paniccia <kevin.paniccia@gmail.com>
2018-05-28 19:40:36 +0000

    updated app.js to prevent page from redirecting after submit request from form

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,17 +5,16 @@
  */
 
 // Test / driver code (temporary). Eventually will get this from the server.
-
-$(document).ready(function () {
+// var moment = require('moment');
 
 const data = [
   {
     "user": {
       "name": "Newton",
       "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
       },
       "handle": "@SirIsaac"
     },
@@ -28,11 +27,12 @@ const data = [
     "user": {
       "name": "Descartes",
       "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
       },
-      "handle": "@rd" },
+      "handle": "@rd"
+    },
     "content": {
       "text": "Je pense , donc je suis"
     },
@@ -42,9 +42,9 @@ const data = [
     "user": {
       "name": "Johann von Goethe",
       "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
       },
       "handle": "@johann49"
     },
@@ -55,50 +55,85 @@ const data = [
   }
 ];
 
-function renderTweets(tweets) {
-  console.log(tweets);
+$(document).ready(function () {
 
-  // for (var content of tweets) {
-    
-  // }
-  var newTweet = createTweetElement(data[0]);
-  $('.showTweets').append(newTweet); 
+  var $form = $('form').on('submit', function(event){
+    event.preventDefault();
+    console.log($form.serialize());
+  });
+  function renderTweets(tweets) {
+    console.log(tweets);
+
+    // for (var content of tweets) {
+
+    // }
+    // var newTweet = createTweetElement(tweet);
+    // $('.showTweets').append(newTweet); 
 
 
 
 
-  
-  // loops through tweets
+
+    // loops through tweets
     // calls createTweetElement for each tweet
     // takes return value and appends it to the tweets container
-}
+  }
 
-function createTweetElement(tweet) {
-  let $tweet = $('<article>').addClass('tweet');
+  function createTweetElement(tweet) {
+    let $tweet = $('<section>').addClass('showTweets');
+
+    let $header = $('<header>');
+
+    let $avatar = $('<img>').addClass('avatar').attr('src', tweet.user.avatars.small); //style is not yet created (still inline)
+
+    let $tweetername = $('<span>').addClass('tweetername')
+    $tweetername.text(tweet.user.name);
+
+    let $tweetholla = $('<span>').addClass('tweetholla')
+    $tweetholla.text(tweet.user.handle);
+
+    let $tweetbod = $('<section>').addClass('tweetbod');
+
+    let $tweettimer = $('<footer>').addClass('tweettimer')
+    $tweettimer.text(tweet.created_at); 
+
+    let $i_flag = $('<i>').addClass('fas fa-flag');
+
+    let $i_rt = $('<i>').addClass('fas fa-retweet');
+
+    let $i_heart = $('<i>').addClass('far fa-heart');
+    
+    $header.append($avatar);
+    $header.append($tweetername);
+    $header.append($tweetholla);
+    $tweetbod.text(tweet.content.text);
+    $tweettimer.append($i_flag);
+    $tweettimer.append($i_rt);
+    $tweettimer.append($i_heart);
+    
+    $tweet.append($header);
+    $tweet.append($tweetbod);
+    $tweet.append($tweettimer);
+    // console.log($header);
+    $('.container').append($tweet);
+    console.log('test');
+
+    // //FOOTER PART
+    // var footer 
+    // var img 
+
+    // footer.append(img);
+
+    // tweet.append(footer)
 
 
-  //create a header
-  var header 
-  //create an image
-  var image
-
-  header.append(image);
-
-  tweet.append(header);
-
-  //FOOTER PART
-  var footer 
-  var img 
-  footer.append(img);
-
-  tweet.append(footer)
-
-  
-  return $tweet;
-}
+    // return $tweet;
+  }
+  data.forEach(tweet => {
+    createTweetElement(tweet);
+  });
 
 
+  // renderTweets(data);
 
-renderTweets(data);
-
-};
+});
